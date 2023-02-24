a3bb5925ce52957d51a9f67e9c6bf608bd4bddf1
Gabriela <gabrielarazoroldan@gmail.com>
2023-02-24 01:24:59 -0600

    add: reusable card component

---
+++ src/components/shared/card/card.style.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const CardContainer = styled.div`
+    width: 100%;
+    border-radius: 7px;
+    border: 1px solid #f0f0f0;
+    background-color: white;
+    box-sizing: border-box;
+
+    ${({ space }) => space && `margin-left: 16px; margin-right: 16px;`}
+    ${({ height }) => height && `height: ${height}px;`}
+    ${({ selected }) => selected && `box-shadow: rgba(0, 0, 0, 0.07) 0px 1px 2px, rgba(0, 0, 0, 0.07) 0px 2px 4px, rgba(0, 0, 0, 0.07) 0px 4px 8px, rgba(0, 0, 0, 0.07) 0px 8px 16px, rgba(0, 0, 0, 0.07) 0px 16px 32px, rgba(0, 0, 0, 0.07) 0px 32px 64px;`}
+`;
