665b7abfa7a89b5b79bdb10d771d3341f3bafe5f
Gabriela <gabrielarazoroldan@gmail.com>
2023-02-24 13:34:50 -0600

    style: pokemonDetails style

---
+++ src/pages/pokemonDetails.style.js
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+export const Text = styled.ul`
+    color: #a8abae;
+    list-style-type: circle;
+    li {
+        font-size: 1rem;
+        font-weight: 100;
+    }
+`;
+
+export const Name = styled.h1`
+    text-align: center;
+    text-transform: capitalize;
+    font-size: 2rem;
+    font-weight: 400;
+    color: #73787c;
+`;
+
+export const SubTitle = styled.h2`
+    text-align: center;
+    font-size: 1.5rem;
+    font-weight: 200;
+    color: #73787c;
+`;
+
+export const Container = styled.div`
+    height: 100vh;
+    background-color: #eff2f5;
+    padding-top: 3em;
+`;
