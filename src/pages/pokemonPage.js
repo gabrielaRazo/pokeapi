80b386582c11f90efde005eab19876fbd2f2ad8f
Gabriela <gabrielarazoroldan@gmail.com>
2023-02-24 13:34:26 -0600

    refactor: pokemonDetails first version

--- src/pages/pokemonPage.js
+++ src/pages/pokemonPage.js
@@ -6,52 +6,75 @@ import Col from "../components/shared/col/col";
 import Image from "../components/shared/image/image";
 import Row from "../components/shared/row/row";
 import { Icon } from "@iconify/react";
+import IconWrap from "../components/shared/iconWrap/iconWrap";
+import { Container, Name, SubTitle, Text } from "./pokemonDetails.style";
+import Tooltip from "../components/shared/tooltip/tooltip";
 
 const PokemonDetails = () => {
     const navigate = useNavigate();
     let { id } = useParams();
     const dispatch = useDispatch();
     const infoPokemon = useSelector((state) => state.dasboardReducer.infoPokemon);
+
     useEffect(() => {
         dispatch({
             type: "GET_POKEMON_ID_REQUEST",
             id: id,
         });
     }, [id, dispatch]);
-    console.log("infoPokemon", infoPokemon);
+    console.log("id", id);
+
     return (
-        <Row center>
-            <Col lg={4} md={4} sm={2} xs={2} space>
-                <Icon icon="mdi:arrow-back-circle" onClick={() => navigate("/")} />
-            </Col>
-            <Col lg={7} md={7} sm={8} xs={8}>
-                <Card shadow>
-                    <Row center>
-                        <Col lg={6} md={6} sm={6} xs={6} space>
-                            <h1>{infoPokemon.name}</h1>
-                        </Col>
-                        <Col lg={2} md={2} sm={6} xs={6}>
-                            <h2>Type</h2>
-                            <ul>
-                                {infoPokemon.types.map((item) => (
-                                    <li>{item.type.name}</li>
-                                ))}
-                            </ul>
-                        </Col>
-                        <Col lg={6} md={6} sm={10} xs={10}>
-                            <Image src={infoPokemon.sprites.back_default} alt="back_default" />
-                            <Image src={infoPokemon.sprites.back_female} alt="back_female" />
-                            <Image src={infoPokemon.sprites.back_shiny} alt="back_shiny" />
+        <Container>
+            <Row center>
+                <Col lg={1} md={1} sm={1} xs={1} space>
+                    <Tooltip text="Return to previews page">
+                        <IconWrap width={"30px"} heigth={"30px"} clickable>
+                            <Icon icon="mdi:arrow-back-circle" onClick={() => navigate("/")} />
+                        </IconWrap>
+                    </Tooltip>
+                </Col>
+                <Col lg={7} md={7} sm={10} xs={10}>
+                    <Card shadow>
+                        <Row center>
+                            <Col lg={5} md={5} sm={5} xs={5} center>
+                                <Image src={infoPokemon.sprites.front_default} alt="front_default" />
+                                <Name>{infoPokemon.name}</Name>
+                            </Col>
+                            <Col lg={6} md={6} sm={12} xs={12} center>
+                                <Row center>
+                                    <Col lg={5} md={5} sm={12} xs={12}>
+                                        <SubTitle style={{ textAlign: "center" }}>Type</SubTitle>
+                                        <Text style={{ textAlign: "center" }}>
+                                            {infoPokemon.types.map((item) => (
+                                                <li> {item.type.name},</li>
+                                            ))}
+                                        </Text>
+                                    </Col>
+                                    <Col lg={5} md={5} sm={12} xs={12} space>
+                                        <SubTitle style={{ textAlign: "center" }}>Abilities</SubTitle>
+                                        <Text style={{ textAlign: "center" }}>
+                                            {infoPokemon.abilities.map((item) => (
+                                                <li> {item.ability.name},</li>
+                                            ))}
+                                        </Text>
+                                    </Col>
+                                </Row>
+                            </Col>
+                        </Row>
+                        <Row center>
                             <Image src={infoPokemon.sprites.back_shiny_female} alt="back_shiny_female" />
-                            <Image src={infoPokemon.sprites.front_default} alt="front_default" />
                             <Image src={infoPokemon.sprites.front_female} alt="front_female" />
-                            <Image src={infoPokemon.sprites.front_shiny} alt="front_shiny" />
+                            <Image src={infoPokemon.sprites.back_default} alt="back_default" />
                             <Image src={infoPokemon.sprites.front_shiny_female} alt="front_shiny_female" />
-                        </Col>
-                    </Row>
-                </Card>
-            </Col>
-        </Row>
+                            <Image src={infoPokemon.sprites.back_female} alt="back_female" />
+                            <Image src={infoPokemon.sprites.front_shiny} alt="front_shiny" />
+                            <Image src={infoPokemon.sprites.back_shiny} alt="back_shiny" />
+                        </Row>
+                    </Card>
+                </Col>
+            </Row>
+        </Container>
     );
 };
 
