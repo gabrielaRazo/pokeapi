5de9b53ee1a74c5dc551eb56a64955544466bb8d
Gabriela <gabrielarazoroldan@gmail.com>
2023-02-24 15:28:11 -0600

    refactor: pokemonPage and pokemonDetails
    pokemonDetails is an UI component while pokemonPage contains all the logic

--- src/components/pokemonDetails/pokemonDetails.js
+++ src/components/pokemonDetails/pokemonDetails.js
@@ -1,10 +1,37 @@
 import React from "react";
+import Col from "../shared/col/col";
+import Image from "../shared/image/image";
+import Row from "../shared/row/row";
+import { Name, SubTitle, Text } from "./pokemonDetails.style";
 
-const PokemonDetails = () => {
+const PokemonDetails = ({ infoPokemon }) => {
     return (
-        <div>
-            <h1>Hola</h1>
-        </div>
+        <Row>
+            <Col lg={5} md={5} sm={5} xs={5} center>
+                <Name>{infoPokemon.name}</Name>
+                <Image src={infoPokemon.sprites.front_default} alt="front_default" />
+            </Col>
+            <Col lg={6} md={6} sm={12} xs={12}>
+                <Row>
+                    <Col lg={5} md={5} sm={12} xs={12}>
+                        <SubTitle>Type</SubTitle>
+                        <Text>
+                            {infoPokemon.types.map((item) => (
+                                <li> {item.type.name}</li>
+                            ))}
+                        </Text>
+                    </Col>
+                    <Col lg={5} md={5} sm={12} xs={12} space>
+                        <SubTitle>Abilities</SubTitle>
+                        <Text>
+                            {infoPokemon.abilities.map((item) => (
+                                <li> {item.ability.name}</li>
+                            ))}
+                        </Text>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
     );
 };
 
--- src/pages/pokemonPage.js
+++ src/pages/pokemonPage.js
@@ -5,12 +5,11 @@ import Card from "../components/shared/card/card";
 import Col from "../components/shared/col/col";
 import Image from "../components/shared/image/image";
 import Row from "../components/shared/row/row";
-import { Icon } from "@iconify/react";
-import IconWrap from "../components/shared/iconWrap/iconWrap";
-import { Container, Name, SubTitle, Text } from "./pokemonDetails.style";
-import Tooltip from "../components/shared/tooltip/tooltip";
+import { Container } from "../components/pokemonDetails/pokemonDetails.style";
+import BackButton from "../components/shared/backButton/backButton";
+import PokemonDetails from "../components/pokemonDetails/pokemonDetails";
 
-const PokemonDetails = () => {
+const PokemonPage = () => {
     const navigate = useNavigate();
     let { id } = useParams();
     const dispatch = useDispatch();
@@ -22,44 +21,20 @@ const PokemonDetails = () => {
             id: id,
         });
     }, [id, dispatch]);
-    console.log("id", id);
+
+    const onClickReturn = () => {
+        navigate("/");
+    };
 
     return (
         <Container>
             <Row center>
-                <Col lg={1} md={1} sm={1} xs={1} space>
-                    <Tooltip text="Return to previews page">
-                        <IconWrap width={"30px"} heigth={"30px"} clickable>
-                            <Icon icon="mdi:arrow-back-circle" onClick={() => navigate("/")} />
-                        </IconWrap>
-                    </Tooltip>
-                </Col>
                 <Col lg={7} md={7} sm={10} xs={10}>
                     <Card shadow>
-                        <Row center>
-                            <Col lg={5} md={5} sm={5} xs={5} center>
-                                <Image src={infoPokemon.sprites.front_default} alt="front_default" />
-                                <Name>{infoPokemon.name}</Name>
-                            </Col>
-                            <Col lg={6} md={6} sm={12} xs={12} center>
-                                <Row center>
-                                    <Col lg={5} md={5} sm={12} xs={12}>
-                                        <SubTitle style={{ textAlign: "center" }}>Type</SubTitle>
-                                        <Text style={{ textAlign: "center" }}>
-                                            {infoPokemon.types.map((item) => (
-                                                <li> {item.type.name},</li>
-                                            ))}
-                                        </Text>
-                                    </Col>
-                                    <Col lg={5} md={5} sm={12} xs={12} space>
-                                        <SubTitle style={{ textAlign: "center" }}>Abilities</SubTitle>
-                                        <Text style={{ textAlign: "center" }}>
-                                            {infoPokemon.abilities.map((item) => (
-                                                <li> {item.ability.name},</li>
-                                            ))}
-                                        </Text>
-                                    </Col>
-                                </Row>
+                        <Row>
+                            <BackButton onClick={onClickReturn} />
+                            <Col lg={10} md={10} sm={10} xs={10}>
+                                <PokemonDetails infoPokemon={infoPokemon} />
                             </Col>
                         </Row>
                         <Row center>
@@ -78,4 +53,4 @@ const PokemonDetails = () => {
     );
 };
 
-export default PokemonDetails;
+export default PokemonPage;
